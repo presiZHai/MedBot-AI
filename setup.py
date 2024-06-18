b2ef68f06f00c34078c13db8fe466ba0d7615c12
Olawale Ogundeji <abiodungndj@gmail.com>
2024-06-18 18:34:19 +0100

    setup file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Medical Chatbot",
+    version = "0.0.1",
+    author= "Olawale A. Ogundeji",
+    author_email= "abiodungndj@gmail.com",
+    packages = find_packages(),
+    install_requires = [],
+    
+)
